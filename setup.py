32f8aae21a27719262a04294f1349e3cc72ea0bb
Pilar Marinas <pilar.marinas@upcnet.es>
2017-09-15 10:58:16 +0200

    Varis

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(
         'Products.PloneFormGen',
         'collective.z3cform.datagridfield',
         'souper',
-        'collective.polls'
+        'collective.polls',
+        'ulearn.js'
     ],
     extras_require={
         'test': [
