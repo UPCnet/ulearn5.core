c0f6d010fecdb853c670a5d74d50cb424c4d0e14
Pilar Marinas <pilar.marinas@upcnet.es>
2018-03-05 10:49:02 +0100

    Extender personal information

--- src/ulearn5/core/userschema.py
+++ src/ulearn5/core/userschema.py
@@ -3,13 +3,10 @@ from zope import schema
 from zope.interface import Interface
 from zope.interface import implements
 from plone.namedfile.field import NamedBlobImage
-# from mrs.max.userdataschema import IEnhancedUserDataSchema
-from mrs5.max.userdataschema import EnhancedUserDataPanelAdapter
+# from mrs.max.userdataschema import IUlearnUserSchema
+# from mrs5.max.userdataschema import EnhancedUserDataPanelAdapter
 # from mrs.max.userdataschema import UserDataSchemaProvider
 
-# from plone.app.users.browser.formlib import FileUpload
-# from plone.app.users.userdataschema import IUserDataSchemaProvider
-from plone.app.users.schema import ICombinedRegisterSchema
 from plone.app.users.schema import checkEmailAddress
 
 from zope.interface import alsoProvides
@@ -20,8 +17,6 @@ from ulearn5.core.adapters.portrait import convertSquareImage
 import urllib
 from OFS.Image import Image
 
-from ulearn5.core import _
-
 from five import grok
 
 from repoze.catalog.catalog import Catalog
@@ -31,60 +26,23 @@ from souper.interfaces import ICatalogFactory
 from souper.soup import NodeAttributeIndexer
 from zope.interface import implementer
 
+from ulearn5.core.interfaces import IUlearn5CoreLayer
 
-class IUlearnUserSchema(Interface):
-    """ Redefinition of all the fields because of the ordering """
+from plone.app.users.browser.userdatapanel import UserDataPanel, UserDataPanelAdapter
 
-    fullname = schema.TextLine(
-        title=_(u'label_full_name', default=u'Full Name'),
-        description=_(u'help_full_name_creation',
-                      default=u'Enter full name, e.g. John Smith.'),
-        required=True)
+from plone.supermodel import model
+from plone.z3cform.fieldsets import extensible
+from zope.component import adapts
+from z3c.form import field
+from plone.app.users.browser.account import AccountPanelSchemaAdapter
+from plone.app.users.browser.register import BaseRegistrationForm
 
-    email = schema.ASCIILine(
-        title=_(u'label_email', default=u'E-mail'),
-        description=u'',
-        required=True,
-        constraint=checkEmailAddress)
+from ulearn5.core import _
 
-    home_page = schema.TextLine(
-        title=_(u'label_homepage', default=u'Home page'),
-        description=_(u'help_homepage',
-                      default=u'The URL for your external home page, '
-                      'if you have one.'),
-        required=False)
+import cgi
 
-    description = schema.Text(
-        title=_(u'label_biography', default=u'Biography'),
-        description=_(u'help_biography',
-                      default=u'A short overview of who you are and what you '
-                      'do. Will be displayed on your author page, linked '
-                      'from the items you create.'),
-        required=False)
 
-    location = schema.TextLine(
-        title=_(u'label_location', default=u'Location'),
-        description=_(u'help_location',
-                      default=u'Your location - either city and '
-                      'country - or in a company setting, where '
-                      'your office is located.'),
-        required=False)
-
-    portrait = NamedBlobImage(
-        title=_(u'label_portrait', default=u'Portrait'),
-        description=_(
-            u'help_portrait',
-            default=u'To add or change the portrait: click the "Browse" '
-                    u'button; select a picture of yourself. Recommended '
-                    u'image size is 75 pixels wide by 100 pixels tall.'
-        ),
-        required=False)
-
-    pdelete = schema.Bool(
-        title=_(u'label_delete_portrait', default=u'Delete Portrait'),
-        description=u'',
-        required=False)
-
+class IUlearnUserSchema(model.Schema):
     twitter_username = schema.TextLine(
         title=_(u'label_twitter', default=u'Twitter username'),
         description=_(u'help_twitter',
@@ -115,64 +73,68 @@ class IUlearnUserSchema(Interface):
     )
 
 
-class UlearnUserSchema(object):
-    implements(ICombinedRegisterSchema)
+class UlearnUserDataSchemaAdapter(AccountPanelSchemaAdapter):
+    schema = IUlearnUserSchema
 
-    def getSchema(self):
-        """
-        """
-        return IUlearnUserSchema
+    # Aqui añadiremos los get y set de nuestro campos
+    # def get_telefon(self):
+    #     return self._getProperty('telefon')
+
+    # def set_telefon(self, value):
+    #     return self._setProperty('telefon', value)
+
+    # telefon = property(get_telefon, set_telefon)
 
 
-class ULearnUserDataPanelAdapter(EnhancedUserDataPanelAdapter):
+class UlearnUserDataPanelExtender(extensible.FormExtender):
+    adapts(Interface, IUlearn5CoreLayer, UserDataPanel)
+    def update(self):
+        fields = field.Fields(IUlearnUserSchema)
 
-    def __init__(self, context):
-        """ Load MAX avatar in portrait.
+        # fields = fields.omit('telefon') # Si queremos quitar alguno de los que hemos añadido
+        # self.remove('home_page') # Si queremos quitar los de la base (plone.app.users)
 
-        """
-        super(EnhancedUserDataPanelAdapter, self).__init__(context)
+        self.add(fields)
 
-        try:
-            from plone.protect.interfaces import IDisableCSRFProtection
-            alsoProvides(self.request, IDisableCSRFProtection)
-        except:
-            pass
 
-        maxclient, settings = getUtility(IMAXClient)()
-        foto = maxclient.people[self.context.id].avatar
-        imageUrl = foto.uri
+class UlearnRegistrationPanelExtender(extensible.FormExtender):
+    adapts(Interface, IUlearn5CoreLayer, BaseRegistrationForm)
+    def update(self):
+        fields = field.Fields(IUlearnUserSchema)
 
-        portrait = urllib.urlretrieve(imageUrl)
+        self.add(fields)
 
-        scaled, mimetype = convertSquareImage(portrait[0])
-        portrait = Image(id=self.context.id, file=scaled, title='')
 
-        membertool = getToolByName(self.context, 'portal_memberdata')
-        membertool._setPortrait(portrait, self.context.id)
-        import transaction
-        transaction.commit()
 
-    def get_ubicacio(self):
-        return self._getProperty('ubicacio')
 
-    def set_ubicacio(self, value):
-        return self.context.setMemberProperties({'ubicacio': value})
-    ubicacio = property(get_ubicacio, set_ubicacio)
+# class ULearnUserDataPanelAdapter(AccountPanelSchemaAdapter):
 
-    def get_telefon(self):
-        return self._getProperty('telefon')
+    # def __init__(self, context):
+    #     """ Load MAX avatar in portrait.
 
-    def set_telefon(self, value):
-        return self.context.setMemberProperties({'telefon': value})
-    telefon = property(get_telefon, set_telefon)
+    #     """
+    #     super(EnhancedUserDataPanelAdapter, self).__init__(context)
 
-    def get_language(self):
-        return self._getProperty('language')
+    #     try:
+    #         from plone.protect.interfaces import IDisableCSRFProtection
+    #         alsoProvides(self.request, IDisableCSRFProtection)
+    #     except:
+    #         pass
 
-    def set_language(self, value):
-        return self.context.setMemberProperties({'language': value})
+    #     maxclient, settings = getUtility(IMAXClient)()
+    #     foto = maxclient.people[self.context.id].avatar
+    #     imageUrl = foto.uri
 
-    language = property(get_language, set_language)
+    #     portrait = urllib.urlretrieve(imageUrl)
+
+    #     scaled, mimetype = convertSquareImage(portrait[0])
+    #     portrait = Image(id=self.context.id, file=scaled, title='')
+
+    #     membertool = getToolByName(self.context, 'portal_memberdata')
+    #     membertool._setPortrait(portrait, self.context.id)
+    #     import transaction
+    #     transaction.commit()
+
 
 
 @implementer(ICatalogFactory)
