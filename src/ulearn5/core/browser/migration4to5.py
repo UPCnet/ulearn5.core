dded0b3448f7a9fb97fc39a475345fb078b55ad5
Iago López Fernández <iago.lopez@upcnet.es>
2019-02-11 12:25:38 +0100

    Hacer parametrizable la vista migrationfixfolderviews

--- src/ulearn5/core/browser/migration4to5.py
+++ src/ulearn5/core/browser/migration4to5.py
@@ -488,7 +488,18 @@ class migrationUsersProfiles(grok.View):
 
 
 class migrationFixFolderViews(grok.View):
-    """ Aquesta vista canvia totes les vistes folder_extended de les carpetes per listing_view. La vista folder_extender no existeix en Plone 5 """
+    """ Aquesta vista canvia totes les vistes folder_extended de les carpetes per listing_view. La vista folder_extender no existeix en Plone 5
+
+[ Paràmetres ]
+old: Obligatori, vista a cambiar.
+new: Obligatori, nova vista que volem.
+list: Opcional, no aplica el canvi i llista el contiguts que utilitzen la vista antigua.
+
+[ Exemples ]
+/migrationfixfolderviews?old=folder_extended&new=listing_view
+/migrationfixfolderviews?old=folder_extended&new=listing_view&list
+"""
+
     grok.name('migrationfixfolderviews')
     grok.context(IPloneSiteRoot)
     grok.require('cmf.ManagePortal')
@@ -500,16 +511,23 @@ class migrationFixFolderViews(grok.View):
         except:
             pass
 
-        original = 'folder_extended'
-        target = 'listing_view'
+        if 'old' not in self.request.form or 'new' not in self.request.form:
+            return 'Cal afegir els paràmetres old i new.\n\nEx:\n/migrationfixfolderviews?old=folder_extended&new=listing_view'
+
+        original = self.request.form['old']
+        target = self.request.form['new']
+
+        msg = ''
         for brain in self.context.portal_catalog(portal_type=('Folder', 'privateFolder')):
             obj = brain.getObject()
             if getattr(obj, "layout", None) == original:
                 logger.info('- Actualitzat: ' + obj.absolute_url())
-                obj.setLayout(target)
+                msg += obj.absolute_url() + '\n'
+                if 'list' not in self.request.form:
+                    obj.setLayout(target)
 
         logger.info('- Ha finalitzat el procés.')
-        return 'Ha finalitzat el procés.'
+        return msg + '\nHa finalitzat el procés.'
 
 
 class listContentsLocalRolesBlock(grok.View):
