03b22ea9eb60c0e5c36d9f3ab4b6e3442662ad08
Iago López Fernández <iago.lopez@upcnet.es>
2019-07-16 14:48:03 +0200

    Añadir nuevo widget Fieldset h5

--- src/ulearn5/core/widgets/fieldset_widget.py
+++ src/ulearn5/core/widgets/fieldset_widget.py
@@ -63,3 +63,21 @@ class H4FieldsetWidget(z3c.form.browser.text.TextWidget):
 @zope.interface.implementer(z3c.form.interfaces.IFieldWidget)
 def H4FieldsetFieldWidget(field, request):
     return z3c.form.widget.FieldWidget(field, H4FieldsetWidget(request))
+
+
+class H5FieldsetWidget(z3c.form.browser.text.TextWidget):
+    zope.interface.implementsOnly(IFieldsetWidget)
+
+    klass = u'fieldset-widget'
+    input_template = ViewPageTemplateFile('templates/h5_fieldset.pt')
+
+    def render(self):
+        if self.mode == z3c.form.interfaces.DISPLAY_MODE:
+            return self.input_template(self)
+        else:
+            return self.input_template(self)
+
+
+@zope.interface.implementer(z3c.form.interfaces.IFieldWidget)
+def H5FieldsetFieldWidget(field, request):
+    return z3c.form.widget.FieldWidget(field, H5FieldsetWidget(request))
