4de17adc6b98901a622c2832891a726fb5090b29
Iago López Fernández <iago.lopez@upcnet.es>
2019-11-22 09:37:19 +0100

    people_literal: Añadir opción Quién es quién

--- src/ulearn5/core/controlpanel.py
+++ src/ulearn5/core/controlpanel.py
@@ -358,7 +358,7 @@ class IUlearnControlPanelSettings(model.Schema):
     people_literal = schema.Choice(
         title=_(u'people_literal'),
         description=_(u'Literals que identifiquen als usuaris de les comunitats i les seves aportacions.'),
-        values=[_(u'thinnkers'), _(u'persones'), _(u'participants'), _(u'collegiates')],
+        values=[_(u'thinnkers'), _(u'persones'), _(u'participants'), _(u'collegiates'), _(u'who_is_who')],
         required=True,
         default=_(u'persones'))
 
