80d356941205e5f8c28152f6bd668af7ede3fdaa
Pilar Marinas <pilar.marinas@upcnet.es>
2020-04-29 08:46:27 +0200

    Solucionar notificacion activity stream archivo protegido

--- src/ulearn5/core/hooks.py
+++ src/ulearn5/core/hooks.py
@@ -180,10 +180,16 @@ def Added(content, event):
         # or the content is created externaly through apps via the upload ws
         return
 
-    if content.portal_type == 'News Item' or content.portal_type == 'ExternalContent':
+    if content.portal_type == 'News Item':
         if event.transition is None:
             return
         elif event.transition.id != 'publicaalaintranet':
+            return
+
+    if content.portal_type == 'ExternalContent':
+        if event.transition is None:
+            return
+        elif event.transition.id != 'publishtointranet':
             return
 
     username, oauth_token = getUserOauthToken(pm)
