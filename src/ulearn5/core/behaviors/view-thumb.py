264d7cc26966be83222395094ce37cf5ed531cce
Iago López <ilopezsmx2011@gmail.com>
2023-11-13 15:47:33 +0100

    [FIX] thumbnail-url imagenes png con transparencia

--- src/ulearn5/core/behaviors/view-thumb.py
+++ src/ulearn5/core/behaviors/view-thumb.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+from PIL import Image
 from Products.Five.browser import BrowserView
-import unicodedata
 
+from io import BytesIO
+
+import unicodedata
 
 class Thumbnail(BrowserView):
 
@@ -11,24 +14,32 @@ class Thumbnail(BrowserView):
 
     def __call__(self):
         context = self.context
+
         if getattr(context, 'thumbnail_image', None) and context.portal_type != 'Image':
-            img_data = context.thumbnail_image.open().read()
             contentType = context.thumbnail_image.contentType
             filename = unicodedata.normalize('NFKD', context.thumbnail_image.filename).encode("ascii", "ignore").decode("ascii")
-
-            self.request.response.setHeader('content-type', contentType)
-            self.request.response.setHeader(
-                'content-disposition', 'inline; filename=' + str(filename.encode('utf-8')))
-            self.request.response.setHeader('content-length', len(img_data))
+            img_open = context.thumbnail_image.open()
         else:
-            # Si no te la Thumb, que no peti i doni la gran...
-            img_data = context.image.open().read()
+            # Si no te la Thumb, que no peti i doni la gran si la té
+            if not context.image:
+                return False
+
             contentType = context.image.contentType
             filename = context.image.filename
+            img_open = context.image.open()
 
-            self.request.response.setHeader('content-type', contentType)
-            self.request.response.setHeader(
-                'content-disposition', 'inline; filename=' + str(filename.encode('utf-8')))
-            self.request.response.setHeader('content-length', len(img_data))
+        img_data = img_open.read()
 
-        return img_data
+        img = Image.open(img_open)
+        if contentType == 'image/png' and img.mode != "RGBA":
+            img = img.convert("RGBA")
+
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+
+        self.request.response.setHeader('content-type', contentType)
+        self.request.response.setHeader(
+            'content-disposition', 'inline; filename=' + str(filename.encode('utf-8')))
+        self.request.response.setHeader('content-length', len(img_data))
+
+        return buffer.getvalue()
