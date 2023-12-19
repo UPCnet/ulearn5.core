1fae032dbb42ddf0eb4862e87da7a8bc72144772
Alberto Durán <alberto.duran@ithinkupc.com>
2023-12-19 07:53:44 +0100

    [FIX] Speed up /api/communities/{com}/documents without heavy unused properties

--- src/ulearn5/core/api/communities.py
+++ src/ulearn5/core/api/communities.py
@@ -976,7 +976,7 @@ class Documents(REST):
         brain = sortedObj['obj'].getObject()
         obj_url = sortedObj['obj'].getURL()
         internal = True
-        content_type = image = raw_file = raw_image = type_next_obj = None
+        type_next_obj = None
         if brain.portal_type == u'Link':
             internal = True if 'resolveuid' in brain.remoteUrl else False
             if internal:
@@ -986,27 +986,16 @@ class Documents(REST):
                 obj_url = next_obj.absolute_url()
             else:
                 obj_url = brain.remoteUrl
-        elif brain.portal_type == 'Image':
-            image = brain.image.filename
-            raw_image = b64encode(brain.image.data) if brain.image.data else None
-            content_type = brain.image.contentType
-        elif brain.portal_type == 'File':
-            raw_file = b64encode(brain.file.data) if brain.file.data else None
-            content_type = brain.file.contentType
         elif brain.portal_type == u'External Content':
             obj_url = brain.absolute_url() + '/@@download/' + brain.filename
         # TODO: eliminar campos duplicados cuando se deje de usar uTalk
         community = dict(absolute_url=obj_url,
-                         content_type=content_type,
                          description=brain.Description(),
                          id=brain.id,
                          external_url=not internal,
-                         image=image,
                          internal=internal,  # Delete
                          path='/'.join(brain.getPhysicalPath()),
                          portal_type=brain.portal_type,
-                         raw_file=raw_file,
-                         raw_image=raw_image,
                          state=sortedObj['obj'].review_state,
                          title=brain.title,
                          type=brain.portal_type,  # Delete
