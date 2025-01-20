86fc17d379261607151c43466f4413e174e3019e
Alberto Durán <alberto.duran@ithinkupc.com>
2025-01-20 14:17:30 +0100

    [UPD] inspector

--- src/ulearn5/core/browser/inspector.py
+++ src/ulearn5/core/browser/inspector.py
@@ -1,56 +1,54 @@
 # -*- coding: utf-8 -*-
-from five import grok
+from Products.Five.browser import BrowserView
 from zope.component.hooks import getSite
-
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-
-from ulearn5.core.interfaces import IUlearn5CoreLayer
-
 
 import inspect
 import importlib
 
 
-MODULES_TO_INSPECT = ['base5.core.setup',
-                      'ulearn5.core.browser.setup',
-                      'ulearn5.core.browser.migration4to5']
+MODULES_TO_INSPECT = [
+    "base5.core.setup",
+    "ulearn5.core.browser.setup",
+]
 
-grok.templatedir("inspector_templates")
 
-
-class clouseau(grok.View):
-    grok.name('clouseau')
-    grok.template('clouseau')
-    grok.context(IPloneSiteRoot)
-    grok.layer(IUlearn5CoreLayer)
-    grok.require('cmf.ManagePortal')
+class clouseau(BrowserView):
 
     def get_helpers(self):
-        portal = getSite()
-        app = portal.restrictedTraverse('/')
+        absolute_url = getSite().absolute_url()
 
-        plone_site = []
-        application = []
-        migration4to5 = []
-        elastic = []
-        ldap = []
+        result = {
+            "plone_site": [],
+            "elastic": [],
+            "ldap": [],
+        }
 
         for module in MODULES_TO_INSPECT:
             themodule = importlib.import_module(module)
             members = inspect.getmembers(themodule, inspect.isclass)
 
             for name, klass in members:
-                if grok.View in klass.__bases__:
-                    if 'LDAP' in name or 'ldap' in name:
-                        ldap.append(dict(url='{}/{}'.format(portal.absolute_url(), getattr(klass, 'grokcore.component.directive.name', name.lower())), description=klass.__doc__))
-                    elif 'Elastic' in name or 'elastic' in name:
-                        elastic.append(dict(url='{}/{}'.format(portal.absolute_url(), getattr(klass, 'grokcore.component.directive.name', name.lower())), description=klass.__doc__))
-                    elif module == 'ulearn5.core.browser.migration4to5':
-                        migration4to5.append(dict(url='{}/{}'.format(portal.absolute_url(), getattr(klass, 'grokcore.component.directive.name', name.lower())), description=klass.__doc__))
-                    elif getattr(klass, 'grokcore.component.directive.context').getName() == 'IApplication':
-                        application.append(dict(url='{}/{}'.format(app.absolute_url(), getattr(klass, 'grokcore.component.directive.name', name.lower())), description=klass.__doc__))
+                if "ulearn5." in str(klass):
+                    if "LDAP" in name or "ldap" in name:
+                        result["ldap"].append(
+                            dict(
+                                url="{}/{}".format(absolute_url, name),
+                                description=klass.__doc__,
+                            )
+                        )
+                    elif "Elastic" in name or "elastic" in name:
+                        result["elastic"].append(
+                            dict(
+                                url="{}/{}".format(absolute_url, name),
+                                description=klass.__doc__,
+                            )
+                        )
                     else:
-                        plone_site.append(dict(url='{}/{}'.format(portal.absolute_url(), getattr(klass, 'grokcore.component.directive.name', name.lower())), description=klass.__doc__))
+                        result["plone_site"].append(
+                            dict(
+                                url="{}/{}".format(absolute_url, name),
+                                description=klass.__doc__,
+                            )
+                        )
 
-
-        return (plone_site, application, migration4to5, ldap, elastic)
+        return result
