3d837bfb90049f9f7b04cf1916950a16e7e82373
pilar.marinas <pilar.marinas@upcnet.es>
2025-04-08 18:30:50 +0200

    Solucionar que funcione el servicio api/event

--- src/ulearn5/core/services/event.py
+++ src/ulearn5/core/services/event.py
@@ -8,11 +8,12 @@ from plone.dexterity.utils import createContentInContainer
 from plone.restapi.services import Service
 from ulearn5.core.services import (MethodNotAllowed, ObjectNotFound,
                                    UnknownEndpoint, check_methods,
-                                   check_required_params)
+                                   check_required_params, check_roles)
 
 from ulearn5.core.html_parser import replaceImagePathByURL
 from ulearn5.core.html_parser import HTMLParser
 from ulearn5.core.html_parser import getCommunityNameFromObj
+import pytz
 
 
 logger = logging.getLogger(__name__)
@@ -51,15 +52,14 @@ class Event(Service):
 
     @check_methods(methods=['GET', 'POST'])
     def reply(self):
-        """ Returns navigation for required context. """
         method = self.request.get('method')
         if method == 'GET':
-            self.reply_get()
+            return self.reply_get()
         elif method == 'POST':
-            self.reply_post()
+            return self.reply_post()
+        raise MethodNotAllowed(f"Unknown method: {method}")
 
-        raise MethodNotAllowed(f"Method not allowed {method}")
-
+    @check_roles(roles=['Member', 'Manager', 'Api'])
     @check_required_params(params=['eventuid'])
     def reply_get(self):
         brain = self.get_brain({'UID': self.request.form.get('eventuid')})
@@ -91,6 +91,7 @@ class Event(Service):
             raise ObjectNotFound(f"Event item with uid {self.request.form.get('eventuid', None)} not found.")
         return {"data": results, "code": 200}
 
+    @check_roles(roles=['Manager', 'Api'])
     @check_required_params(params=['eventid', 'title', 'description', 'body', 'start', 'end'])
     def reply_post(self):
         response = self.create_event()
@@ -115,11 +116,16 @@ class Event(Service):
         title = self.request.form.get('title')
         body = self.request.form.get('body')
         description = self.request.form.get('description')
+        community_id = self.request.form.get('community')
+        pc = api.portal.get_tool('portal_catalog')
+        if community_id:
+            community = pc.unrestrictedSearchResults(portal_type='ulearn.community', id=community_id)
+            events_url = community[0].getObject()['events']
+        else:
+            portal_url = api.portal.get()
+            events_url = portal_url['events']
 
-        portal_url = api.portal.get()
-        events_url = portal_url['events']
-
-        brains = self.get_brain({'id': event_id})
+        brains = pc.unrestrictedSearchResults(portal_type='Event', id=event_id)
 
         if not brains:
             new_event = createContentInContainer(
@@ -145,6 +151,10 @@ class Event(Service):
 
     def parse_datetime(self, datetime_str):
         try:
-            return datetime.strptime(datetime_str, '%d/%m/%Y/%H:%M')
-        except (ValueError, TypeError):
+            # Si es un timestamp, conviértelo a datetime
+            if datetime_str.isdigit():
+                return datetime.fromtimestamp(float(datetime_str), pytz.timezone("Europe/Madrid"))
+            # Si es una fecha en formato '%d/%m/%Y/%H:%M', conviértela
+            return datetime.strptime(datetime_str, '%d/%m/%Y/%H:%M').replace(tzinfo=pytz.timezone("Europe/Madrid"))
+        except (ValueError, TypeError, AttributeError):
             return None
