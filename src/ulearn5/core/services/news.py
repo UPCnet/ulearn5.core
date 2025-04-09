f14e0196bc749482e44daef9b59bc4e4fbd2c59b
pilar.marinas <pilar.marinas@upcnet.es>
2025-04-09 12:54:55 +0200

    Solucionar que funcione el servicio api/news

--- src/ulearn5/core/services/news.py
+++ src/ulearn5/core/services/news.py
@@ -3,12 +3,12 @@ import logging
 
 from plone import api
 from plone.restapi.services import Service
-from ulearn5.core.services import check_methods
+from ulearn5.core.services import check_methods, check_roles
 from ulearn5.core.services.new import New
 from ulearn5.core.services.utils import show_news_in_app
 
-# from ulearn5.core.html_parser import (HTMLParser, getCommunityNameFromObj,
-#                                       replaceImagePathByURL)
+from ulearn5.core.html_parser import (HTMLParser, getCommunityNameFromObj,
+                                      replaceImagePathByURL)
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +22,7 @@ class News(Service):
             - page: (str) The page number to retrieve.
             - path: (str) Community name
             - external_or_internal: ['zoom_in', 'zoom_out']
-        
+
         Get all News by "X-Oauth-Username"
 
     - Subpaths allowed: YES
@@ -36,7 +36,7 @@ class News(Service):
         self.page = kwargs.get('page', None)
         self.path = kwargs.get('path', None)
         self.external_or_internal = kwargs.get('external_or_internal', None)
-        
+
     def handle_subpath(self, subpath):
         """ Function used to spread the request to the corresponding subpath """
         if not subpath:
@@ -47,22 +47,22 @@ class News(Service):
 
         if handler_class:
             handler = handler_class(self.context, self.request)
-            return handler.handle_subpath(subpath[1:]) 
-        
+            return handler.handle_subpath(subpath[1:])
+
         # No subpath is considered wrong, as a New will be
-        # updated if it's found, and created otherwise 
+        # updated if it's found, and created otherwise
         # This means any string in its path will be the New id
         kwargs = {'new_id': next_segment}
         new_handler = New(self.context, self.request, **kwargs)
         return new_handler.handle_subpath(subpath[1:])
 
-
+    @check_roles(roles=['Member', 'Manager', 'Api'])
     @check_methods(methods=['GET'])
     def reply(self):
         results = []
         pagination_page = self.request.form.get('page', None)
         total_news = 0
-        
+
         if show_news_in_app():
             news, total_news = self.find_news()
             news, more_items = self.paginate_news(news, total_news, pagination_page)
@@ -75,11 +75,11 @@ class News(Service):
         info = {
             'items': results,
             'more_items': more_items,
-            'total_news': total_news 
+            'total_news': total_news
         }
         return {"data": info, "code": 200}
 
-    
+
     def find_news(self):
         query = self.build_query()
         news = api.content.find(**query)
@@ -118,7 +118,7 @@ class News(Service):
                 more_items = True
 
         return news, more_items
-    
+
     def process_news_item(self, item):
         value = item.getObject()
         date = self.process_date(value)
@@ -150,16 +150,16 @@ class News(Service):
     def process_date(self, item):
         if getattr(item, 'effective_date', None):
             return item.effective_date.strftime("%d/%m/%Y")
-        
+
         return item.creation_date.strftime("%d/%m/%Y")
 
     def process_text(self, item):
         if getattr(item, 'text', None):
             text = replaceImagePathByURL(item.text.output)
             return HTMLParser(text)
-        
+
         return None
-    
+
     def process_image(self, item):
         filename = None
         contentType = None
@@ -169,5 +169,5 @@ class News(Service):
             filename = item.image.filename
             contentType = item.image.contentType
             raw_image = item.absolute_url() + '/thumbnail-image'
-        
+
         return filename, contentType, raw_image
